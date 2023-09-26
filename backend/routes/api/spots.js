907e124afd8392d8023abef6455ad0c3bd7f3d70
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-09-26 19:16:13 +0200

    something

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -496,6 +496,7 @@ router.get("/:spotId/bookings", async (req, res) => {
 
 
 
+
 //return spot based on spot ID
 router.get("/:spotId", async (req, res) => {
   const spot = await Spot.findByPk(req.params.spotId, {
